2fedd1ac0d92df45ec009c8faca30a8c6498eaa1
izygabs <80975143+izygabs@users.noreply.github.com>
2024-03-25 08:43:47 +0100

    Deploy Routing

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongooseConnection = require("./mongoConnect");
+const db = require("./mongoConnect");
 const port = 4000;
 const app = express();
 const cors = require("cors");
-const route = require("./routes/allRoutes");
+// const route = require("./routes/allRoutes");
 
-app.use(cors());
+// app.use(cors());
 
-app.use(express.json());
+// app.use(express.json());
+
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-app.use(route);
+// app.use(route);
 
 app.get("/", (req, res) => {
   res.send("Hello World, from Naija01");
-});
-
-app.listen(port, async () => {
-  await mongooseConnection();
-  console.log(`Server is running on ${port}`);
 });
 
 // mongooseConnection.on("open", async () => {
@@ -36,25 +38,34 @@ app.listen(port, async () => {
 //   console.error("MongoDB connection error:", error);
 // });
 
-// app.post("/addUser", async (req, res) => {
-//   try {
-//     let collection = await db.getDb().collection("users");
-//     let newDocument = req.body;
-//     newDocument.date = new Date();
-//     let result = await collection.insertOne(newDocument);
-//     console.log("req", req.body);
-//     res.status(204).send(result);
-//   } catch (error) {
-//     console.error("Error adding user:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
+app.post("/addUser", async (req, res) => {
+  try {
+    let collection = await db.getDb().collection("users");
+    let newDocument = req.body;
+    newDocument.date = new Date();
+    let result = await collection.insertOne(newDocument);
+    console.log("req", req.body);
+    res.status(204).send(result);
+  } catch (error) {
+    console.error("Error adding user:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/getUsers", async (req, res) => {
+  let collection = await db.getDb().collection("users");
+  let results = await collection
+    .find({})
+
+    .toArray();
+  res.send(results).status(200);
+});
+
+// app.listen(port, async () => {
+//   await mongooseConnection();
+//   console.log(`Server is running on ${port}`);
 // });
 
-// app.get("/getUsers", async (req, res) => {
-//   let collection = await db.getDb().collection("users");
-//   let results = await collection
-//     .find({})
-
-//     .toArray();
-//   res.send(results).status(200);
-// });
+app.listen(port, function () {
+  console.log("Server is listening at port:" + port);
+});
--- mongoConnect.js
+++ mongoConnect.js
@@ -1,67 +1,53 @@
-// const { MongoClient } = require("mongodb");
-// require("dotenv").config();
-// const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-// const connectString =
-//   `mongodb+srv://Testserver:${password}@devcluster.4n9t8fi.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster` ||
-//   `mongodb://localhost:27017/Naija01`;
-
-// const client = new MongoClient(connectString);
-
-// let database; // variable to store the reference to the database
-
-// async function connectToDatabase() {
-//   try {
-//     await client.connect();
-//     console.log("Connection established");
-//     database = client.db("test_naija"); // assign the database object
-//   } catch (e) {
-//     console.error(e);
-//   }
-// }
-
-// // Call the asynchronous function to establish the connection
-// connectToDatabase();
-
-// module.exports = {
-//   getDb: function () {
-//     return database;
-//   },
-//   getClient: function () {
-//     return client;
-//   },
-// };
-
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 require("dotenv").config();
-
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
 const connectString =
   `mongodb+srv://Testserver:${password}@devcluster.4n9t8fi.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster` ||
   `mongodb://localhost:27017/Naija01`;
 
-// mongoose.connect(connectString, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+const client = new MongoClient(connectString);
 
-// const db = mongoose.connection;
+let database; // variable to store the reference to the database
 
-// db.on("error", console.error.bind(console, "MongoDB connection error:"));
-// db.once("open", () => {
-//   console.log("MongoDB connection established");
-// });
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connection established");
+    database = client.db("test_naija"); // assign the database object
+  } catch (e) {
+    console.error(e);
+  }
+}
 
-// module.exports = db;
+// // Call the asynchronous function to establish the connection
+connectToDatabase();
 
-const mongooseConnection = () => {
-  return mongoose
-    .connect(connectString)
-    .then(() => {
-      console.log(`Connected to ${connectString}`);
-    })
-    .catch((err) => {
-      console.log(`Connection to ${connectString} failed ${err}`);
-    });
+module.exports = {
+  getDb: function () {
+    return database;
+  },
+  getClient: function () {
+    return client;
+  },
 };
 
-module.exports = mongooseConnection;
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+// const connectString =
+//   `mongodb+srv://Testserver:${password}@devcluster.4n9t8fi.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster` ||
+//   `mongodb://localhost:27017/Naija01`;
+
+// const mongooseConnection = () => {
+//   return mongoose
+//     .connect(connectString)
+//     .then(() => {
+//       console.log(`Connected to ${connectString}`);
+//     })
+//     .catch((err) => {
+//       console.log(`Connection to ${connectString} failed ${err}`);
+//     });
+// };
+
+// module.exports = mongooseConnection;
